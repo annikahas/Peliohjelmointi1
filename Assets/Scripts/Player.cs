5f6ab08c2c042123dc9381eec31cb8c7cbf601ce
annikahas <43463674+annikahas@users.noreply.github.com>
2021-03-25 22:47:34 +0200

    Enemy & pickup
    Added enemies and pickups

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb2d;
-    public float speed;
+    public Rigidbody2D rb2d;
+    public float speed = 10f;
+
+    Vector2 movement;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
+        rb2d.AddForce(movement * speed);*/
+
+        rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+        
     }
 }
---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject prefab1, prefab2, prefab3;
+    public float spawnrate = 2f;
+    float nextSpawn = 0f;
+    int whatToSpawn;
+
+    private void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            whatToSpawn = Random.Range(1, 6);
+            Debug.Log(whatToSpawn);
+
+            switch (whatToSpawn)
+            {
+                case 1:
+                    Instantiate(prefab1, transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(prefab2, transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(prefab3, transform.position, Quaternion.identity);
+                    break;
+            }
+
+            nextSpawn = Time.time + spawnrate;
+        }
+    }
+}
